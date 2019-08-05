075fd603bab8de029bd9eccae8e81bdf8cc5ac4e
RajasekharMugada <rajasekhar.mugada@gmail.com>
2019-08-05 13:29:09 +0530

    Added source file for python hello world project
    Python variables are introduced in this project

---
+++ hello_world_python.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Rajasekhar Mugada
+@brief : Python basics
+"""
+
+#Python variables
+
+#strings
+dt_str = "Hello world ! - Python"
+
+#numbers : int/long/float/complx
+dt_num1 = 123.45
+dt_num2 = 12 + 3.45j
+
+#list
+dt_list = ['hi', 1, 2, 3, 4.56]
+
+#tuple
+dt_tuple = ('hi', 1, 2, 3, 4.56) 
+
+#Dictionary
+dt_dict = {'key':'value', 'id':2, 10:1234}
+
+#set : add, union, intersect, difference, clear
+dt_set = {'hi', 1, 2, 3, 4.56, 'hi', 1, 2, 3, 4.56}
+
+
+print ("String : ", dt_str )
+print ("Numbers : float ex: ", dt_num1, " complex ex: ", dt_num2 )
+print ("List : ", dt_list)
+print ("Tuple : ", dt_tuple)
+print ("Dictionary : ", dt_dict)
+print ("Set : ", dt_set)
