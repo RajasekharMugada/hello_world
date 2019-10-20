83bfe892b95ee8f50ace4e58cdd04f6b71a37a01
RajasekharMugada <rajasekhar.mugada@gmail.com>
2019-10-20 15:10:54 +0530

    More examples added for accessing data type variables

--- hello_world_python.py
+++ hello_world_python.py
@@ -28,7 +28,7 @@ dt_set = {'hi', 1, 2, 3, 4.56, 'hi', 1, 2, 3, 4.56}
 
 print ("String : ", dt_str )
 print ("Numbers : float ex: ", dt_num1, " complex ex: ", dt_num2 )
-print ("List : ", dt_list)
-print ("Tuple : ", dt_tuple)
-print ("Dictionary : ", dt_dict)
+print ("List : ", dt_list, "  list's 0th index element : ", dt_list[0])
+print ("Tuple : ", dt_tuple, "  tuple's 0th index element : ", dt_tuple[0])
+print ("Dictionary : ", dt_dict, "  dictionary value for key 10 : ", dt_dict[10])
 print ("Set : ", dt_set)
